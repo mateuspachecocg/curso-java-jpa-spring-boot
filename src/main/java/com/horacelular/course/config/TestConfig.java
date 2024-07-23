121928e614dd885d785538b66b3e2a246d2bea7c
F7021234 Mateus Pacheco Uchoa <mateus.uchoa@bb.com.br>
2024-07-23 10:58:47 -0300

    Payment, one-to-one association

--- src/main/java/com/horacelular/course/config/TestConfig.java
+++ src/main/java/com/horacelular/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.horacelular.course.entities.Category;
 import com.horacelular.course.entities.Order;
 import com.horacelular.course.entities.OrderItem;
+import com.horacelular.course.entities.Payment;
 import com.horacelular.course.entities.Product;
 import com.horacelular.course.entities.User;
 import com.horacelular.course.entities.enums.OrderStatus;
@@ -74,10 +75,16 @@ public class TestConfig implements CommandLineRunner {
     OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
     OrderItem oi4 = new OrderItem(o3, p5, 2, p3.getPrice());
 
+    Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+    o1.setPayment(pay1);
+
+    orderRepository.save(o1);
   }
 }
--- src/main/java/com/horacelular/course/entities/Order.java
+++ src/main/java/com/horacelular/course/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.horacelular.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 /**
@@ -39,6 +41,9 @@ public class Order implements Serializable {
 
   @OneToMany(mappedBy = "id.order")
   private Set<OrderItem> items = new HashSet<>();
+
+  @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) // Mapeando as duas entidades com mesmo Id
+  private Payment payment;
 
   public Order() {
   }
@@ -77,7 +82,6 @@ public class Order implements Serializable {
   }
 
   @ManyToOne
-
   public User getClient() {
     return client;
   }
@@ -88,6 +92,14 @@ public class Order implements Serializable {
 
   public Set<OrderItem> getItems() {
     return items;
+  }
+
+  public Payment getPayment() {
+    return payment;
+  }
+
+  public void setPayment(Payment payment) {
+    this.payment = payment;
   }
 
   @Override
@@ -119,4 +131,5 @@ public class Order implements Serializable {
   public String toString() {
     return "Order [id=" + id + ", moment=" + moment + ", client=" + client + "]";
   }
+
 }
