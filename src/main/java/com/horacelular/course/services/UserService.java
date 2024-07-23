71fcbe9f0c652eaf062717c90019c85f386ce53c
F7021234 Mateus Pacheco Uchoa <mateus.uchoa@bb.com.br>
2024-07-23 13:46:39 -0300

    Exception handling - update

--- src/main/java/com/horacelular/course/services/UserService.java
+++ src/main/java/com/horacelular/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.horacelular.course.repositories.UserRepository;
 import com.horacelular.course.services.exceptions.DatabaseException;
 import com.horacelular.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 /**
  * UserService
  */
@@ -43,9 +45,13 @@ public class UserService {
   }
 
   public User update(Long id, User obj) {
-    User entity = repository.getReferenceById(id);
-    updateData(entity, obj);
-    return repository.save(entity);
+    try {
+      User entity = repository.getReferenceById(id);
+      updateData(entity, obj);
+      return repository.save(entity);
+    } catch (EntityNotFoundException e) {
+      throw new ResourceNotFoundException(id);
+    }
   }
 
   private void updateData(User entity, User obj) {
